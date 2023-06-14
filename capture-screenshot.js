71d8472cfc90eeda6dff94aff1fa0489249b2388
Kiriti Desarkar <kiriti.desarkar.mails@gmail.com>
2023-06-15 00:11:08 +0530

    Update capture-screenshot.js

--- capture-screenshot.js
+++ capture-screenshot.js
@@ -1,20 +1,36 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: "new" });
+  // Fetch the HTML content of the LeetCode profile page
+  const response = await axios.get('https://leetcode.com/your-username');
+  const html = response.data;
+
+  // Load the HTML content into Cheerio
+  const $ = cheerio.load(html);
+
+  // Find the dashboard section using a specific selector
+  const dashboardSection = $('.panel.panel-primary').first();
+
+  // Extract the HTML content of the dashboard section
+  const dashboardHtml = $.html(dashboardSection);
+
+  // Launch a headless Chrome browser using Puppeteer
+  const browser = await puppeteer.launch();
+
+  // Open a new page
   const page = await browser.newPage();
 
-  // Adjust the viewport size if necessary
+  // Set the viewport size if necessary
   await page.setViewport({ width: 1280, height: 800 });
 
-  // Navigate to your LeetCode profile
-  await page.goto('https://leetcode.com/kiritidesarkar/', { timeout: 60000 });
+  // Set the HTML content of the page to the dashboard section
+  await page.setContent(dashboardHtml);
 
-  // Wait for the profile page to load
-  await page.waitForNavigation();
+  // Capture a screenshot of the dashboard section
+  await page.screenshot({ path: 'leetcode-dashboard.png' });
 
-  // Capture the full-page screenshot
-  await page.screenshot({ path: 'leetcode-profile.png', fullPage: true });
-
+  // Close the browser
   await browser.close();
 })();
