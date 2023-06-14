99c5bd7cf770397cb0888913b15a02ad033cf51b
Kiriti Desarkar <kiriti.desarkar.mails@gmail.com>
2023-06-15 00:05:07 +0530

    Update capture-screenshot.js

--- capture-screenshot.js
+++ capture-screenshot.js
@@ -1,14 +1,14 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: "new" });
   const page = await browser.newPage();
 
   // Adjust the viewport size if necessary
   await page.setViewport({ width: 1280, height: 800 });
 
   // Navigate to your LeetCode profile
-  await page.goto('https://leetcode.com/kiritidesarkar/');
+  await page.goto('https://leetcode.com/kiritidesarkar/', { timeout: 60000 });
 
   // Wait for the profile page to load
   await page.waitForNavigation();
